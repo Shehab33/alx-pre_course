79eb0d3e12c11b2e125e679bbddc2707b49ccd3e
Shehab Mohammed <shehabjaheen@gmail.com>
2023-07-19 04:10:26 -0700

    print_alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+include "main.h"
+/**
+ * main - the man Fn
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+}
+
+	print_alphabet();
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,16 @@
+/**
+ * main - void print small alphabet
+ * Dsecription: a void Fn that prints alphabets and then prints newline
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+	_putchar(i);
+	i++;
+	}
+	_putchar('\n');
+}
