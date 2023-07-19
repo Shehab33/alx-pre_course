509ac80e53345da0a434da04d0370a8661f81812
Shehab Mohammed <shehabjaheen@gmail.com>
2023-07-19 02:55:10 -0700

    _putchar Fn

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <main.h>
+#include <unistd.h>
+
 /**
- * _putchar - is function that take char and print it with \n after it
- * @c: the character which will print
- * Return: 0 (success)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-
-	printf("c\n");
-	return (0);
+	return (write(1, &c, 1));
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#define main_h
-#ifndef main_h
 int _putchar(char c);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,5 +9,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
-#endif
