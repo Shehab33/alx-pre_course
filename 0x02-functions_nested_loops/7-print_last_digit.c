bb856a74f8b8680be4b28c50b00cfc4277726878
Shehab Mohammed <shehabjaheen@gmail.com>
2023-07-19 08:27:38 -0700

    last-digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* print_last_digit - prints last digit of any number
+* @n: the number argument
+* Return: the last digit
+*/
+int print_last_digit(int n)
+{
+
+	res = abs(n) % 10;
+
+	return (_putchar(res));
+}
