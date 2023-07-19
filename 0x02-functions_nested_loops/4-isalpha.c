63dfdd07ffe63b0c6f8453784349d87a97b9a640
Shehab Mohammed <shehabjaheen@gmail.com>
2023-07-19 07:38:32 -0700

    _isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include <ctype.h>
 /**
- * _islower - is Fn that take char and return 1 when it is lowercase or Uppercase
+ * _ialpha - is Fn that takes R-1  when it is lowercase or Uppercase
  * @c: the int argument of Fn _isalpha
  * Return: 1 (when char is lowercase or uppercase) otherwise 0
  */
