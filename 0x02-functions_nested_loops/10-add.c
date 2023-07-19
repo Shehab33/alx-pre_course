693e5561b4ace9af346b371c2472d36fac5c5668
Shehab Mohammed <shehabjaheen@gmail.com>
2023-07-19 09:55:42 -0700

    add Fn

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Fn that add 2 integers
+ * @n1: the first number
+ * @n2: the second number
+ * Return: sum of 2 values
+ */
+int add(int n1, int n2)
+{
+	int res = n1 + n2;
+
+	return (res);
+}
