f9f39447822154b552b760640992e9abba50ac62
Shehab Mohammed <shehabjaheen@gmail.com>
2023-07-19 03:13:48 -0700

    _putchar Fn

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * mian - the main Fn to test all comming
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	return 0;
+}
