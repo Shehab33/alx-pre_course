f9d3ef96581b9e74ab2d8ee8ec3dc0f5e4f98293
Shehab Mohammed <shehabjaheen@gmail.com>
2023-07-19 05:04:47 -0700

    Print-alphabets_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  a void Fn that prints alphabets 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+
+	int j = 0, i;
+
+	While(j <= 9)
+	{
+	int i = 97;
+
+	while (i <= 122)
+	{
+
+	_putchar(i);
+	i++;
+	_putchar('\n');
+	}
+	}
+}
