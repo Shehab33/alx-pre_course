186a411a1f3dd36da0fa95cc7fb24d2e52de3b77
Shehab Mohammed <shehabjaheen@gmail.com>
2023-07-19 05:54:14 -0700

    _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+/**
+ * _islower - is Fn that take char and return 1 when it is lowercase
+ * @c: the int argument of Fn _islower
+ * Return: 1 (when char is lowercase) otherwise 0
+ */
+ int _islower(int c)
+ {
+
+	if (islower(c))
+	{
+	return 1;
+	}
+	return 0;
+}
