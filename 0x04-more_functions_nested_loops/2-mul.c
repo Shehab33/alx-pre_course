689027cc3cf5f4e956c7ae6509bb1c75a83b010c
Shehab Mohammed <shehabjaheen@gmail.com>
2023-07-21 02:31:13 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * mul - is Fn that mulitiplies 2-num
+ * @a: is the first num
+ * @b: is the second num
+ * Return: the result of multiplies
+ */
+int mul(int a, int b)
+{
+	int res = a * b;
+
+	return (res);
+}
