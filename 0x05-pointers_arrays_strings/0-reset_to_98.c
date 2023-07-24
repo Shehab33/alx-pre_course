457f831aea29184ac42eab8bf6863073b724e5ee
Shehab Mohammed <shehabjaheen@gmail.com>
2023-07-24 14:24:12 -0700

    Point to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - Fn is just update the value to 98
+ * @n: is a pointer we use to set the value to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
