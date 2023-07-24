30eb849f6b89eef174d0b45d7b9117c8b9a8fafc
Shehab Mohammed <shehabjaheen@gmail.com>
2023-07-24 15:08:21 -0700

    length os string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strlen - Fn that returns the length of string
+ * @s: the string we want to return its length
+ * Return: int the length of the string
+ */
+int _strlen(char *s)
+{
+	int strsize = strlen(s);
+
+	return (strsize);
+}
