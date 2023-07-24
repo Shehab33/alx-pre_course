749c114fe27c734fb07f459b0ba5c843a196bbf2
Shehab Mohammed <shehabjaheen@gmail.com>
2023-07-24 15:15:31 -0700

    Print string using puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Fn that prints string followed by newline
+ * @str: the string  that we will print
+ * Return: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
